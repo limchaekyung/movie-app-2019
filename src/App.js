7dbcdc89697f881a23c7d06a0b6cbcb4295c865b
limchaekyung <corud505@naver.com>
2022-08-19 23:24:43 +0900

    Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }
+  getMovies = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json")
+  }
+  componentDidMount() {
+    this.getMovies()
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>
+  }
 }
 
 export default App;
